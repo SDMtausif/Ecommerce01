81af74cbeb750112c7f1288aa9489af9c9c2b3ca
SDMtausif <sdmnd5873@gmail.com>
2025-06-14 02:03:16 +0600

    Updated feature X

--- Ecommerce01/src/Components/Header/Header.jsx
+++ Ecommerce01/src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='text-center bg-black py-[15px]'>    
     <Container>
-    <p className='text-white font-Poppins'>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+    <p className='text-white font-Poppins text-[12px] md:text-[14px] md:font-Poppins md:leading-[21px] md:font-normal lg:text-[18px]'>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
     <span className='ml-2 font-Poppins underline' ><Link to="/">Shop Now</Link></span></p>
     </Container>
     </div>
--- Ecommerce01/src/Components/Nabvar/Navbar.jsx
+++ Ecommerce01/src/Components/Nabvar/Navbar.jsx
@@ -1,16 +1,50 @@
-import React from 'react'
-import Container from '../Container/Container'
+import React from 'react';
+import Container from '../Container/Container';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/Exclusive.png'
+import { CiSearch } from "react-icons/ci";
+import { CiHeart } from "react-icons/ci";
+import { IoCartOutline } from "react-icons/io5";
+
+
 const Navbar = () => {
   return (
     <div>
-        <Container>
-            <div><Link to="/" >Exclusive</Link></div>
-            <div></div>
-            <div></div>
-        </Container>
+      <Container>
+        <div className="flex justify-between pt-[49px] pb-[21px]">        
+          <div>
+            <Link to="/">
+              <img src={logo} alt="" />
+            </Link>
+          </div> 
+          <div className=' '>
+            <ul className="sm:flex sm:flex-col sm:gap-[30px] md:flex md:flex-row">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/">Contact</Link></li>
+              <li><Link to="/">About</Link></li>
+              <li><Link to="/">Sign Up</Link></li>
+            </ul>
+          </div>
+          <div className="flex items-center gap-6">
+            <div className="relative max-w-[220px]">
+              <input 
+                className="bg-[#F5F5F5] border-0 outline-0 w-full rounded-sm font-Poppins text-[12px] py-[3px] pl-[20px] pr-[70px] text-[#7B7B7B] leading-[18px]" 
+                type="text" 
+                placeholder="What are you looking for?"
+              />
+              <div className="absolute top-1/2 right-[10%] transform -translate-y-1/2 hover:scale-[1.05]">
+                <CiSearch />
+              </div>
+            </div>  
+            <div className="flex gap-4">
+              <div><CiHeart size={32} /></div>
+              <div><IoCartOutline size={32} /></div>
+            </div>          
+          </div>
+        </div>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
